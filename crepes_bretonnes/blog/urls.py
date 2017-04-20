b45917bc5da30fd0d553b72a353776af87442531
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-04-20 16:40:10 +0200

    Create a homepage for the project and another for blog app

--- crepes_bretonnes/blog/urls.py
+++ crepes_bretonnes/blog/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^accueil/$', views.home),
+    url(r'^$', views.home),
     url(r'date/$', views.date_actuelle),
 ]
--- crepes_bretonnes/crepes_bretonnes/urls.py
+++ crepes_bretonnes/crepes_bretonnes/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from blog import views
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('blog.urls')),
+    url(r'^$', views.home),
+    url(r'^blog/', include('blog.urls')),
 ]
