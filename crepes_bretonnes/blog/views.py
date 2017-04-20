10c41ae14518fdcabee0392672b1ce6b7ed2d5b0
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-04-20 16:20:09 +0200

    Create blog views

--- crepes_bretonnes/blog/views.py
+++ crepes_bretonnes/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+def home(request):
+	return render(request, 'blog/accueil.html')
+
+def date_actuelle(request):
+	return render(request, 'blog/date.html', {'date' : datetime.now()})
