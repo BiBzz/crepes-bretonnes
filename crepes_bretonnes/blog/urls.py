5a57c4e092e4771c0d12fbe0811ee7ccebb0c530
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-04-25 16:02:40 +0200

    Update blog home view name

--- crepes_bretonnes/blog/urls.py
+++ crepes_bretonnes/blog/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home),
+    url(r'^$', views.bloghome),
     url(r'date/$', views.date_actuelle),
 ]
--- crepes_bretonnes/blog/views.py
+++ crepes_bretonnes/blog/views.py
@@ -1,8 +1,9 @@
+from django.http import Http404
 from django.shortcuts import render
 from datetime import datetime
 
 # Create your views here.
-def home(request):
+def bloghome(request):
 	return render(request, 'blog/accueil.html')
 
 def date_actuelle(request):
