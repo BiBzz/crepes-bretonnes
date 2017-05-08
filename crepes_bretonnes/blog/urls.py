30a0ddf471fe8c7483c43fe05d6627e764b1e2e4
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-05-09 00:28:26 +0200

    Unify views names

--- crepes_bretonnes/blog/urls.py
+++ crepes_bretonnes/blog/urls.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.bloghome),
-    url(r'^date/$', views.date_actuelle),
-    url(r'^post/(?P<post_id>\d+)$', views.view_post, name="afficher_article"),
-    url(r'^posts/(?P<year>\d{4})/(?P<month>\d{2})$', views.list_posts, name="lister_articles"),
-    url(r'^posts/(?P<month>\d{2})/(?P<year>\d{4})$', views.redirect_list_posts, name="Rediriger_listes_articles"),
+    url(r'^$', views.blog_home, name="blog_home"),
+    url(r'^date/$', views.date),
+    url(r'^post/(?P<post_id>\d+)$', views.view_post, name="view_post"),
+    url(r'^posts/(?P<year>\d{4})/(?P<month>\d{2})$', views.list_posts, name="list_posts"),
+    url(r'^posts/(?P<month>\d{2})/(?P<year>\d{4})$', views.redirect_list_posts, name="redirect_to_list_posts"),
 ]
