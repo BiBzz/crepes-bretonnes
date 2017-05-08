f062b799a9e713e8650a62414729ec6bd34782ce
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-05-09 00:33:22 +0200

    Add slug on posts

--- crepes_bretonnes/blog/models.py
+++ crepes_bretonnes/blog/models.py
@@ -9,6 +9,7 @@ class Category(models.Model):
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
+	slug = models.SlugField(max_length=100)
 	author = models.CharField(max_length=42)
 	content = models.TextField(null=True)
 	date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
