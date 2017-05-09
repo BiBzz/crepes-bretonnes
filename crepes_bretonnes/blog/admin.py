16bf82650ff64e32dc2c9a9c08e9617d0d9f6fba
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-05-09 16:12:32 +0200

    Add excerpt column in posts admin list

--- crepes_bretonnes/blog/admin.py
+++ crepes_bretonnes/blog/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Category, Post
+from django.utils.text import Truncator
 
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-	list_display = ( 'title', 'author', 'date' )
+	list_display = ( 'title', 'author', 'date', 'display_excerpt' )
 	list_filter = ( 'author', 'category', )
 	date_hierarchy = 'date'
 	ordering = ( 'date', )
-	search_fields = ( 'titre', 'contenu' )
+	search_fields = ( 'title', 'content' )
 
+	def display_excerpt(self, post):
+		return Truncator(post.content).chars(40, truncate='...')
+
+
+	display_excerpt.short_description = "Aperçu du contenu"
 
 admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
