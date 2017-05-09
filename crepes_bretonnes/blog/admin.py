c12b5ddff4b9b37f593412c9e0bdadf5dc389983
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-05-09 15:25:07 +0200

    Enhance posts admin interface

--- crepes_bretonnes/blog/admin.py
+++ crepes_bretonnes/blog/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Post, Category
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ( 'title', 'author', 'date' )
+	list_filter = ( 'author', 'category', )
+	date_hierarchy = 'date'
+	ordering = ( 'date', )
+	search_fields = ( 'titre', 'contenu' )
+
+
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
