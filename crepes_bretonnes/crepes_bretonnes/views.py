7c62e09361ba0e64e317b9f73c39359a9d9ed96f
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-05-10 09:28:55 +0200

    Create contact view

--- crepes_bretonnes/crepes_bretonnes/views.py
+++ crepes_bretonnes/crepes_bretonnes/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 # Create your views here.
 def home(request):
-	return render(request, 'accueil.html')
+    return render(request, 'accueil.html')
+
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        subject = form.cleaned_data['subject']
+        message = form.cleaned_data['message']
+        sender_mail = form.cleaned_data['sender_mail']
+        confirmation = form.cleaned_data['confirmation']
+      
+        form_sent = True
+
+    return render(request, 'contact.html', locals())
