45a05bedfa756050ad4be1eab1bfd1e6b717a6a2
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-05-12 09:55:09 +0200

    Add simple validation rule on contact form

--- crepes_bretonnes/crepes_bretonnes/forms.py
+++ crepes_bretonnes/crepes_bretonnes/forms.py
@@ -4,5 +4,16 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100, label="Sujet")
     message = forms.CharField(widget=forms.Textarea)
     sender_mail = forms.EmailField(label="Votre email")
-    confirmation = forms.BooleanField(help_text="Cochez pour recevoir une confirmation", 
-required=False)
+    confirmation = forms.BooleanField(help_text="Cochez pour recevoir une confirmation", required=False)
+
+    def clean(self):
+        cleaned_data = super(ContactForm, self).clean()
+        subject = cleaned_data.get('subject')
+        message = cleaned_data.get('message')
+
+        if subject and message:
+            if "pizza" in subject and "pizza" in message:
+                msg = "Merci de ne pas parler de pizza dans le sujet et le message!"
+                self.add_error("message", msg)
+
+        return cleaned_data
