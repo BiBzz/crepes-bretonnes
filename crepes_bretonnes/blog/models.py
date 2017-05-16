9c66b7d4633637c3f7574cb0114528c507a1ea15
Alexandre BERREBI <alexandre.berrebi@businessagile.eu>
2017-05-16 10:04:43 +0200

    Add Contact model in blog app

--- crepes_bretonnes/blog/models.py
+++ crepes_bretonnes/blog/models.py
@@ -2,18 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-	name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
-	title = models.CharField(max_length=100)
-	slug = models.SlugField(max_length=100)
-	author = models.CharField(max_length=42)
-	content = models.TextField(null=True)
-	date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
-	category = models.ForeignKey('Category')
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    author = models.CharField(max_length=42)
+    content = models.TextField(null=True)
+    date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
+    category = models.ForeignKey('Category')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField
+    picture = models.ImageField(upload_to="pictures/")
+
+    def __str__(self):
+        return self.name
